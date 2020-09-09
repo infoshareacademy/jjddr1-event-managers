e42db71fa1a8f6095086b6efe04b05d8f28a1301
kpodsiadlo <kpodsiadlo@gmail.com>
2020-09-09 15:10:36 +0200

    working on properties

--- src/main/java/com.infoshare.eventmanagers/Main.java
+++ src/main/java/com.infoshare.eventmanagers/Main.java
@@ -1,7 +1,8 @@
-package main.java.com.infoshare.eventmanagers;
+package com.infoshare.eventmanagers;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(main.java.com.infoshare.eventmanagers.EventMgrProperties.getProperties());
         return;
     }
 }

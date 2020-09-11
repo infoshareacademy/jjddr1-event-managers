6af21c1e804847b92e1323ed2066802cdca44af3
kpodsiadlo <kpodsiadlo@gmail.com>
2020-09-11 14:00:41 +0200

    first working version

--- src/main/java/com.infoshare.eventmanagers/Main.java
+++
@@ -1,8 +0,0 @@
-package com.infoshare.eventmanagers;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(main.java.com.infoshare.eventmanagers.EventMgrProperties.getProperties());
-        return;
-    }
-}
---
+++ src/main/java/com/infoshare/eventmanagers/Main.java
@@ -0,0 +1,10 @@
+package com.infoshare.eventmanagers;
+
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        EventMgrProperties eventMgrproperties = new EventMgrProperties();
+        System.out.println(eventMgrproperties.toString());
+    }
+}

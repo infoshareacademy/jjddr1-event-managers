0ea7168d855342299230a098bd1646c7660acebe
kpodsiadlo <kpodsiadlo@gmail.com>
2020-09-11 14:19:57 +0200

    corretions to main.java

--- src/main/java/com/infoshare/eventmanagers/Main.java
+++ src/main/java/com/infoshare/eventmanagers/Main.java
@@ -1,7 +1,5 @@
 package com.infoshare.eventmanagers;
 
-import java.util.Properties;
-
 public class Main {
     public static void main(String[] args) {
         EventMgrProperties eventMgrproperties = new EventMgrProperties();

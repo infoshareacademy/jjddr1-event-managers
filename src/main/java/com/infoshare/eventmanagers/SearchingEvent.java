43771e2c9be63083b1c832116417409e64e0953f
Zuzanna Lesner <zuzanna.lesner@gmail.com>
2020-09-17 21:27:35 +0200

    comments added

--- src/main/java/com/infoshare/eventmanagers/SearchingEvent.java
+++ src/main/java/com/infoshare/eventmanagers/SearchingEvent.java
@@ -10,15 +10,28 @@ import java.util.Scanner;
 import com.infoshare.eventmanagers.Event;
 import com.infoshare.eventmanagers.SaveJson;
 
+/**
+ * A class to search the event from the whole list, by name.
+ */
 public class SearchingEvent {
 
+    /**
+     * A method that takes data from the user and compares it with data,
+     * from a list of events.
+     */
     public void run() {
 
+        /**
+         * Downloading data from the user.
+         */
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Podaj nazwę wydarzenia, które szukasz");
+        System.out.println("Podaj nazwę wydarzenia, które szukasz.");
         String givenEvent = scanner.nextLine();
 
+        /**
+         * Comparing the data entered by the user with the data from the event list.
+         */
         boolean isFound = false;
         for (Event s : Repository.eventList) {
             if (s.getName().contains(givenEvent)) {

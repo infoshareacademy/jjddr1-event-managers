07f2379a7bcdb246db4715558ccaffec594f1c81
Szymon Kiełbiowski <kielbiowski.main@gmail.com>
2020-09-17 19:37:48 +0200

    JavaDoc text added to classes EventListFilter and MenuEventListFilter

--- src/main/java/com/infoshare/eventmanagers/EventListFilter.java
+++ src/main/java/com/infoshare/eventmanagers/EventListFilter.java
@@ -6,6 +6,11 @@ import java.util.stream.Collectors;
 
 public class EventListFilter {
 
+    /**
+     * Method filters list of Events by date provided as parameter.
+     * @param date LocalDate variable provided for list filtering needs.
+     * @return list of filtered Events.
+     */
     static List<Event> filterByDate(LocalDate date) {
         List<Event> filterEvent = SaveJson.saveJsonAsArray()
                 .stream()
@@ -14,6 +19,12 @@ public class EventListFilter {
         return filterEvent;
     }
 
+    /**
+     * Method filters list of Events by a range of dates provided as parameters.
+     * @param fromDate inclusive LocalDate variable defining beginning of a range provided for list filtering needs.
+     * @param toDate inclusive LocalDate variable defining end of a range provided for list filtering needs.
+     * @return list of filtered Events.
+     */
     static List<Event> filterByDate(LocalDate fromDate, LocalDate toDate) {
         List<Event> filterEvent = SaveJson.saveJsonAsArray()
                 .stream()
@@ -23,6 +34,11 @@ public class EventListFilter {
         return filterEvent;
     }
 
+    /**
+     * Method filters list of Events by String organizer provided as parameter.
+     * @param organizer String variable provided for list filtering needs.
+     * @return list of filtered Events.
+     */
     static List<Event> filterByOrganizer(String organizer) {
         List<Event> filterEvent = SaveJson.saveJsonAsArray()
                 .stream()

437b5923225d094ac60efbebfa05bb77f439f494
Sebastian <sbrowarczyk@wp.pl>
2020-09-21 17:08:14 +0200

    Refactoring Menu class and added makeAChoice method in Utils class

--- src/main/java/com/infoshare/eventmanagers/Menu.java
+++ src/main/java/com/infoshare/eventmanagers/Menu.java
@@ -3,16 +3,15 @@ package com.infoshare.eventmanagers;
 import com.infoshare.eventmanagers.favorites.Favorites;
 import com.infoshare.eventmanagers.filter.MenuEventListFilter;
 import com.infoshare.eventmanagers.properties.MenuEventMgrProperties;
+import com.infoshare.eventmanagers.utils.Utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.Scanner;
 
 public class Menu {
 
     private final static Logger LOGGER = LogManager.getLogger(Menu.class);
+    private final String message = "Wybrano opcję: {} \n";
 
-    private Scanner scanner = new Scanner(System.in);
 
     private String[] MENULIST = {"Wyszukiwanie wydarzeń", "Lista wszystkich wydarzeń z widokiem pojedyńczego wydarzenia",
             "Filtrowanie listy wydarzeń", "Edycja konfiguracji", "Pokazywanie najbliższego w czasie ulubionego wydarzenia",
@@ -29,38 +28,34 @@ public class Menu {
     private void showMenu() {
         boolean next = true;
         while (next) {
-            clearScreen();
-            printMenu();
+            Utils.clearScreen();
+            Utils.printMenu(MENULIST);
             LOGGER.info("Dokonaj wyboru :");
-            int choice = Integer.parseInt(scanner.nextLine().trim());
-            if (choice == 0) {
-                return;
-            }
-            switch (choice) {
+            switch (Utils.makeAChoice()) {
                 case 1:
-                    LOGGER.info("Wybrano opcję: {}\n ", MENULIST[0]);
+                    LOGGER.info(message, MENULIST[0]);
                     SearchingEvent newSerchingEvent = new SearchingEvent();
                     newSerchingEvent.run();
 
                     break;
                 case 2:
-                    LOGGER.info("Wybrano opcję: {} \n", MENULIST[1]);
+                    LOGGER.info(message, MENULIST[1]);
                     new EventList().listView();
                     break;
                 case 3:
-                    LOGGER.info("Wybrano opcję: {} \n", MENULIST[2]);
+                    LOGGER.info(message, MENULIST[2]);
                     MenuEventListFilter.showMenu();
                     break;
                 case 4:
-                    LOGGER.info("Wybrano opcję: {} \n", MENULIST[3]);
+                    LOGGER.info(message, MENULIST[3]);
                     MenuEventMgrProperties eventMenu = new MenuEventMgrProperties();
                     eventMenu.displayPropertiesMenu();
                     break;
                 case 5:
-                    LOGGER.info("Wybrano opcję: " + MENULIST[4]);
+                    LOGGER.info(message, MENULIST[4]);
                     break;
                 case 6:
-                    LOGGER.info("Wybrano opcję: " + MENULIST[5]);
+                    LOGGER.info(message, MENULIST[5]);
                     Favorites favorites = new Favorites();
                     favorites.showFavoriteMenu();
                     break;
@@ -71,29 +66,6 @@ public class Menu {
                     LOGGER.info("Brak takiej opcji \n");
             }
         }
-    }
-
-    private void printMenu() {
-        printLine();
-        for (int i = 0; i < MENULIST.length; i++) {
-            LOGGER.info("{}: {}\n", (i + 1), MENULIST[i]);
-        }
-        LOGGER.info("7: Wyjście \n");
-        printLine();
-    }
-
-    public void printLine() {
-        for (int i = 0; i < 80; i++) {
-            LOGGER.info("─");
-        }
-        LOGGER.info("\n");
-    }
-
-    public void clearScreen() {
-
-        LOGGER.info("\033[H\033[2J");
-        LOGGER.info("\n");
-
     }
 
 }
--- src/main/java/com/infoshare/eventmanagers/utils/Utils.java
+++ src/main/java/com/infoshare/eventmanagers/utils/Utils.java
@@ -5,12 +5,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Utils {
 
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
+    private static Scanner scanner = new Scanner(System.in);
 
-    static void printMenu(String[] menuArray) {
+
+    public static void printMenu(String[] menuArray) {
         printLine();
         int i = 0;
         for (; i < menuArray.length; i++) {
@@ -20,21 +23,40 @@ public class Utils {
         printLine();
     }
 
-    static void printLine() {
+    public static void printLine() {
         for (int i = 0; i < 84; i++) {
             STDOUT.info("─");
         }
         STDOUT.info("\n");
     }
 
-    static void clearScreen(){
+    public static void clearScreen() {
         STDOUT.info("\033[H\033[2J");
         STDOUT.info("\n");
     }
 
     // TODO:implement methods in Printer class, then call them in static context here.
-    static void printListByFive(List<Event> eventList){
+    static void printListByFive(List<Event> eventList) {
 
+    }
+
+    /**
+     * Method  for reading input from user, with try-catch block, if user doesn't pass  numbers then  return 0;
+     *
+     * @return a int @choice from scanner, if @parseInt throw a NumberFormatException  method return 0
+     */
+
+
+    public static int makeAChoice() {
+        STDOUT.info("Dokonaj wyboru :");
+        int choice;
+        try {
+            choice = Integer.parseInt(scanner.nextLine().trim());
+
+        } catch (NumberFormatException n) {
+            choice = 0;
+        }
+        return choice;
     }
 
 }

80743a1e4fb7e61d13c443c441ea7563bc90b8c7
Bartłomiej Mitek <bartekmitek@wp.pl>
2020-09-30 14:38:01 +0200

    Task JJDDR1EM-12 (I'm not sure) done

--- src/main/java/com/infoshare/eventmanagers/favorites/Favorites.java
+++ src/main/java/com/infoshare/eventmanagers/favorites/Favorites.java
@@ -3,7 +3,6 @@ package com.infoshare.eventmanagers.favorites;
 import com.infoshare.eventmanagers.Menu;
 import com.infoshare.eventmanagers.model.Event;
 import com.infoshare.eventmanagers.repository.Repository;
-import com.infoshare.eventmanagers.utils.Printer;
 import com.infoshare.eventmanagers.utils.Utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,72 +17,30 @@ public class Favorites {
     private final static Logger LOGGER = LogManager.getLogger(Menu.class);
 
     /**
-     * Declaration and initialization of List of Favorites Events.
-     */
-    List<Event> favoritesList = new ArrayList<>();
-
-    /**
      * Declaration of Scanner with console implementation.
      */
     Scanner scanner = new Scanner(System.in);
 
     /**
-     * Declaration and initialization of String Array, which include available options
-     * in Menu of Favorites.
+     * Declaration and initialization of List of Favorites Events.
      */
-    String[] menuListFavorites = {"Wyświetl listę ulubionych wydarzeń", "Dodaj wydarzenie do listy ulubionych",
-            "Usuń wydarzenie z listy ulubionych" };
-
-    /**
-     * Shows Menu of Favorites Events with all options in switch instruction, which could be
-     * chosen by input in Scanner.
-     */
-    public void showFavoriteMenu() {
-        printFavoriteMenu();
-        boolean next = true;
-        while (next) {
-            int choice = scanner.nextInt();
-            switch (choice) {
-                case 1:
-                    LOGGER.info("Wybrano opcję: " + menuListFavorites[0] + "\n");
-                    viewFavorites();
-                    showFavoriteMenu();
-                    break;
-                case 2:
-                    LOGGER.info("Wybrano opcję: " + menuListFavorites[1] + "\n");
-                    addToFavorites();
-                    showFavoriteMenu();
-                    break;
-                case 3:
-                    LOGGER.info("Wybrano opcję: " + menuListFavorites[2] + "\n");
-                    deleteFromFavorites();
-                    showFavoriteMenu();
-                    break;
-                case 4:
-                    next = false;
-                    break;
-                default:
-                    LOGGER.info("Brak takiej opcji \n");
-            }
-        }
-    }
-
+    List<Event> favoritesList = new ArrayList<>();
 
     /**
      * Prints Menu of Favorites Events.
      */
-    private void printFavoriteMenu() {
+    protected static void printFavoriteMenu() {
         Utils.printLine();
-        Utils.printMenu(menuListFavorites);
+        Utils.printMenu(MenuFavorites.menuListFavorites);
     }
+
 
     /**
      * Searches an event by ID in eventList and add this event to favoritesList.
      */
-    void addToFavorites() {
+    protected void addToFavorites() {
 
-        Printer printer = new Printer(Repository.eventList);
-        printer.listView();
+        Utils.printListByFive(Repository.eventList);
 
         LOGGER.info("Write ID number of an event you want to add: \n");
         int index = scanner.nextInt();
@@ -105,7 +62,7 @@ public class Favorites {
     /**
      * Searches an event by ID in favoritesList and deletes this event.
      */
-    void deleteFromFavorites() {
+    protected void deleteFromFavorites() {
 
         viewFavorites();
         LOGGER.info("Write ID number of an event you want to delete: \n");
@@ -129,7 +86,7 @@ public class Favorites {
     /**
      * Prints every event in favoritesList
      */
-    void viewFavorites() {
+    protected void viewFavorites() {
         for (Event event : favoritesList) {
             event.printAsElement();
         }

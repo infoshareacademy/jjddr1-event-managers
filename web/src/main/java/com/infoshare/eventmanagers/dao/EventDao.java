2822dd11341f89c26c7e5f3b073fefe89cfae786
kpodsiadlo <kpodsiadlo@gmail.com>
2020-11-04 15:01:10 +0100

    3306

--- web/src/main/java/com/infoshare/eventmanagers/dao/EventDao.java
+++ web/src/main/java/com/infoshare/eventmanagers/dao/EventDao.java
@@ -8,6 +8,7 @@ import com.infoshare.eventmanagers.models.Place;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.util.List;
